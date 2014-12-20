09888c785d0a945f58809bddf1862415beba7033
Zach Cotter <cotter.z@husky.neu.edu>
2014-12-20 00:01:04 -0600

    follow tutorial

---
+++ TipCalculatorModel.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class TipCalculatorModel {
+    var total : Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTips = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTips {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+}
